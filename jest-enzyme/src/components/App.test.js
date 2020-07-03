746e5e54eda3a37fec34e83557cfab42aab02a02
Jayashree Nagarajan <njayashree20196@gmail.com>
2020-07-03 17:39:47 +0530

    added BDD for the test suits

--- jest-enzyme/src/components/App.test.js
+++ jest-enzyme/src/components/App.test.js
@@ -2,24 +2,34 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(< App />);
+describe('App', () => {
+    const app = shallow(< App />);
 
-it('renders correctly', () => {
-    expect(app).toMatchSnapshot();
-});
+    it('renders correctly', () => {
+        expect(app).toMatchSnapshot();
+    });
+    
+    it('tests to check empty `gifts` from `state`', () => {
+        expect(app.state().gifts).toEqual([]);
+    });
+    
+    describe('groups the same click of a button', () => {
+        beforeEach(() => {
+            app.find('.btn-app').simulate('click');
+        })
 
-it('tests to check empty `gifts` from `state`', () => {
-    expect(app.state().gifts).toEqual([]);
-});
-
-it('adds a button and checks the `gifts` value', () => {
-    app.find('.btn-app').simulate('click');
-    expect(app.state().gifts).toEqual([{
-        id:1
-    }])
-})
-
-it('creates a div whenever a new gift is added', () => {
-    app.find('.btn-app').simulate('click');
-    expect(app.find('.gift-list').children().length).toEqual(2);
+        afterEach(() => {
+            app.setState({gifts: []})
+        })
+        
+        it('adds a button and checks the `gifts` value', () => {
+            expect(app.state().gifts).toEqual([{
+                id:1
+            }])
+        })
+        
+        it('creates a div whenever a new gift is added', () => {
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        })
+    })
 })
